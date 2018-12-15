ac5f298ee6339f88eb77c4f35470cee7d3c40ca6
spjw99 <jinpark701@gmail.com>
2018-12-15 01:21:18 -0500

    saving

---
+++ public/assets/js/app.js
@@ -0,0 +1,163 @@
+function call_alert(mode, msg){
+    if(mode === "success"){
+        $('.alert').addClass('alert-success').removeClass('danger').text(msg).fadeOut(3000);
+    }else if(mode === "error"){
+        $('.alert').addClass('alert-danger').removeClass('alert-success').text(msg).fadeOut(3000);
+    }
+}
+function modal_open(article_id, title){
+    console.log(article_id);
+    $('#exampleModalCenter').removeClass('fade').show();
+    $('.modal-title').text("Comment - " + decodeURIComponent(title));
+    $('.modal_submit_btn').attr('data-article-id', article_id);
+    $('.comment_content').val("");
+    $('.comment_scr').empty();
+}
+function modal_close(){
+    $('#exampleModalCenter').addClass('fade').hide();
+    $('.modal-title').text("Comment");
+    $('.modal_submit_btn').attr('data-article-id', '');
+    $('.comment_content').val("");
+    $('.comment_scr').empty();
+}
+$(document).ready(function(){
+    /**********************************************************************
+     * CLICK TO GET ALL COMMENTS FOR AN ARTICLE AND OPEN MODAL WITH EDITOR
+     *********************************************************************/
+    $('.comment_btn').on('click', function(){
+        var article_id = $(this).attr('data-id');
+        var title = $(this).attr('data-title');
+
+        var req_data = {article_id : article_id};
+        $.ajax({
+            url : "/api/select_comment",
+            method : "POST",
+            data : req_data
+        })
+        .then((res_data) => {
+            if(res_data.length > 0){
+                res_data.map((comment) => {
+                    // console.log(comment);
+                    var comment_div = $('<div class="border my-3 py-2 pb-3">');
+                    var comment_del_btn = $('<button class="comment_del_btn btn btn-danger float-right">');
+                    comment_del_btn.attr('data-comment-id', comment._id);
+                    comment_del_btn.text("Del");
+
+                    comment_div.html(comment.content);
+                    comment_div.append(comment_del_btn);
+
+                    $('.comment_scr').prepend(comment_div);
+                })
+            }
+        })
+
+        modal_open(article_id, title);
+
+    });
+    $('.modal_close_btn').on('click', function(){
+        modal_close();
+    });
+    /*********************************
+     * CLICK TO SUBMIT COMMENT BUTTON
+     *********************************/
+    $('.modal_submit_btn').on('click', function(){
+        var article_id = $(this).attr('data-article-id');
+        var content = $('.comment_content').val();
+        var req_data = {article_id : article_id, content : content};
+        // console.log("FROM app.js");
+        // console.log(req_data);
+        $.ajax({
+            url : "api/insert_comment",
+            method : "POST",
+            data : req_data
+        })
+        .then((res_data) => {
+            console.log(res_data);
+            $('.comment_content').val("");
+            var comment_div = $('<div class="border my-3 py-2 pb-3">');
+            var comment_del_btn = $('<button class="comment_del_btn btn btn-danger float-right">');
+            comment_del_btn.attr('data-comment-id', res_data._id);
+            comment_del_btn.text("Del");
+
+            comment_div.html(res_data.content);
+            comment_div.append(comment_del_btn);
+
+            $('.comment_scr').prepend(comment_div);
+            call_alert("success", "Comment is succesfully created.");
+        })
+        
+        //modal_close();
+    });
+    $('.modal').on('click', '.comment_del_btn', function(){
+        var this_obj = $(this);
+        var comment_id = $(this).attr('data-comment-id');
+        var req_data = {comment_id : comment_id};
+        $.ajax({
+            url : "/api/delete_comment",
+            method : "POST",
+            data : req_data
+        })
+        .then((res_data) => {
+            if(comment_id === res_data._id){
+                this_obj.parent().remove();
+                call_alert("success", "Comment is succesfully deleted.");
+                return false;
+            }else{
+                call_alert("error", "Hmm... Something is wrong. Please contact to Admin.");
+                return false;
+                //id doesn't match. something is wrong
+            }
+        })
+    })
+    /*********************************
+    * CLICK TO SAVE ARTICLE BUTTON
+    *********************************/
+    $('.save_article_btn').on('click', function(){
+        var this_obj = $(this);
+        var title = $(this).attr('data-title');
+        var link = $(this).attr('data-link');
+        var image = $(this).attr('data-image');
+        var category = $(this).attr('data-category');
+        var req_data = {title : title, link : link, image : image, category : category};
+        $.ajax({
+            method: "POST",
+            url: "/api/save",
+            data: req_data
+            })
+            .then(function(res_data) {
+
+                if(res_data === "added"){
+                    this_obj.removeClass('btn-dark').addClass('btn-success').text("SAVED!!!");
+                    this_obj.parent().parent().fadeOut(2000);
+                    call_alert("success", "Article is succesfully added.");
+                    return false;
+                }else{
+
+                }
+            });
+    });
+    /***************************************
+     * CLICK TO DELETE SAVED ARTICLE BUTTON
+     ****************************************/
+    $('.delete_btn').on('click', function(){
+        var this_obj = $(this);
+        var article_id = $(this).attr('data-id');
+        var req_data = {article_id : article_id};
+        $.ajax({
+            url : "/api/delete_article",
+            method : "POST",
+            data : req_data
+        })
+        .then((res_data) => {
+            if(article_id === res_data._id){
+                this_obj.parent().parent().remove();
+                call_alert("success", "Article is succesfully deleted.");
+                return false;
+            }else{
+                call_alert("error", "Hmm... Something is wrong. Please contact to Admin.");
+                return false;
+                //id doesn't match. something is wrong
+            }
+        })
+    });
+})
---
+++ server.js
@@ -0,0 +1,160 @@
+var express = require("express");
+var exphbs = require("express-handlebars");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var axios = require("axios");
+var cheerio = require("cheerio");
+var helpers = require('handlebars-helpers');
+var helpers_url = helpers.url();
+
+// Require all models
+var db = require("./models");
+
+var PORT = 3000;
+
+// Initialize Express
+var app = express();
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
+
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Connect to the Mongo DB
+mongoose.connect("mongodb://localhost/hw14", { useNewUrlParser: true });
+
+// Routes
+app.get("/", (req, res) => res.render("index"));
+
+/********************
+ * SCRAPPING ARTICLES
+ ********************/
+app.get("/scrape", function(req, res) {
+  axios.get("https://www.allkpop.com/").then(function(response) {
+    var $ = cheerio.load(response.data);
+    
+    var result = {data:[]};
+    
+    $("#more_stories_scr article.list").each(function(i, element) {
+      var title = helpers_url.encodeURI($(element).find(".title").text());
+      var link = $(element).find("a").attr("href");
+      var image = $(element).find("img").attr("src");
+      var category = $(element).find('.category').text();
+        if( title && link && image){
+            result.data.push({
+                title : title,
+                link : "https://www.allkpop.com" + link,
+                image : image,
+                category : category
+            });
+        }
+    });
+    // console.log(result);
+    res.render("index", result);
+  });
+});
+
+
+/*****************************
+ * SELECT SAVED ARTICLE FROM DB
+ *****************************/
+app.get("/saved_articles", function(req, res) {
+  db.Article.find({}).sort( { _id: -1 } )
+    .then((dbArticle) => {
+        var result = {data : dbArticle};
+        //console.log(result);
+      //res.json(dbArticle);
+      res.render("index", result);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+/************************
+ * INSERT ARTICLE INTO DB 
+ ***********************/
+app.post("/api/save", function(req, res){
+  //console.log(req.body);
+  db.Article.create(req.body)
+  .then((dbArticle) => {
+      //console.log(dbArticle);
+      db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbArticle._id }, { new: true });
+      res.json("added");
+  })
+  .catch((err) => {
+      res.json(err);
+  });
+})
+/******************************
+ * DELETE SAVED ARTICLE FROM DB
+ *******************************/
+app.post('/api/delete_article', function(req, res){
+  if(req.body.article_id){
+    db.Article.findByIdAndRemove(req.body.article_id)
+    .then((dbArticle) => {
+        res.json(dbArticle);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+  }
+})
+
+
+
+
+/*************************************
+ * SELECT ALL COMMENTS FOR AN ARTICLE
+ ************************************/
+app.post("/api/select_comment", function(req, res) {
+  db.Comment.find({ article_id: req.body.article_id })
+    .then(function(dbComment) {
+      res.json(dbComment);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+/************************
+ * INSERT NEW COMMENT
+ *********************/
+app.post('/api/insert_comment', function(req, res){
+  // console.log("FROM server.js");
+  // console.log(db);
+  db.Comment.create(req.body)
+  .then((dbComment) => {
+    console.log(dbComment);
+    db.Comment.findOneAndUpdate({ _id: req.params.id }, { note: dbComment._id }, { new: true });
+    res.json(dbComment);
+  })
+  .catch((err) => {
+    res.json(err);
+  })
+})
+/******************************
+ * DELETE COMMENT FROM DB
+ *******************************/
+app.post('/api/delete_comment', function(req, res){
+  if(req.body.comment_id){
+    db.Comment.findByIdAndRemove(req.body.comment_id)
+    .then((dbComment) => {
+        res.json(dbComment);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+  }
+})
+
+// Start the server
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
+});
